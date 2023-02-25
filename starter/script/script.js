1bce00b85d47c1c673ea0ab7136b02dfac13d06d
janeezy <51208612+janeezy@users.noreply.github.com>
2023-02-25 10:10:50 +0000

    2 commit
    commit 2

---
+++ starter/script/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.pragraph');
+document.querySelector('.pragraph').textContent = 'welcome to my page ';
+
+const welcomepage = function () {
+	prompt('how are you feeling today!');
+};
+welcomepage();
